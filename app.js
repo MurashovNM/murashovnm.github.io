7c858d32ffebb26b1459e95826f6b57f7121a8e5
Nikita Murashov <nmurashov@sdvor.com>
2023-02-16 11:26:31 +0500

    Test 7

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ var input_body = $('#body');
 var timerId = setInterval(setNotificationDemoBody, 10000);
 
 function setNotificationDemoBody() {
-    if (input_body.val().search(/^It's found today at \d\d:\d\d$/i) !== -1) {
+    if (input_body.val().search(/^It's sent today at \d\d:\d\d$/i) !== -1) {
         var now = new Date();
-        input_body.val('It\'s found today at ' + now.getHours() + ':' + addZero(now.getMinutes()));
+        input_body.val('It\'s sent today at ' + now.getHours() + ':' + addZero(now.getMinutes()));
     } else {
         clearInterval(timerId);
     }
