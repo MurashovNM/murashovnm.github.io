b7e3697642183815611cf60e473ebd2eaccc2a58
Nikita Murashov <nmurashov@sdvor.com>
2024-08-20 17:25:08 +0500

    add config

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // Firebase Configurations
 var firebaseConfig = {
-    projectId: "fmc-test-14150",
-    messagingSenderId: "636475170145",
+  apiKey: "AIzaSyCRXnah5DNJzyr6DhSRXugUgGY8r9ZgqpY",
+  authDomain: "fmc-test-14150.firebaseapp.com",
+  projectId: "fmc-test-14150",
+  storageBucket: "fmc-test-14150.appspot.com",
+  messagingSenderId: "636475170145",
+  appId: "1:636475170145:web:389d4fb749693008937920"
 };
 // Initializing Firebase App
 firebase.initializeApp(firebaseConfig);
--- sw.js
+++ sw.js
@@ -1,8 +1,12 @@
 importScripts('https://www.gstatic.com/firebasejs/7.14.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.14.0/firebase-messaging.js');
 var firebaseConfig = {
-    projectId: "fmc-test-14150",
-    messagingSenderId: "636475170145",
+  apiKey: "AIzaSyCRXnah5DNJzyr6DhSRXugUgGY8r9ZgqpY",
+  authDomain: "fmc-test-14150.firebaseapp.com",
+  projectId: "fmc-test-14150",
+  storageBucket: "fmc-test-14150.appspot.com",
+  messagingSenderId: "636475170145",
+  appId: "1:636475170145:web:389d4fb749693008937920"
 };
 
 firebase.initializeApp(firebaseConfig);
