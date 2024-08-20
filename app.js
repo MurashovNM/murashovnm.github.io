ec3266c18fde38832cc19666cf8d10c4262bd1a4
Nikita Murashov <nmurashov@sdvor.com>
2024-08-20 21:38:54 +0500

    new fb version

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-firebase.initializeApp({
-    messagingSenderId: '636475170145'
-});
+var firebaseConfig = {
+  apiKey: "AIzaSyCRXnah5DNJzyr6DhSRXugUgGY8r9ZgqpY",
+  authDomain: "fmc-test-14150.firebaseapp.com",
+  projectId: "fmc-test-14150",
+  storageBucket: "fmc-test-14150.appspot.com",
+  messagingSenderId: "636475170145",
+  appId: "1:636475170145:web:389d4fb749693008937920"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 
 var bt_register = $('#register');
--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,9 +1,16 @@
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/7.14.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.14.0/firebase-messaging.js');
 
-firebase.initializeApp({
-  messagingSenderId: '636475170145'
-});
+var firebaseConfig = {
+  apiKey: "AIzaSyCRXnah5DNJzyr6DhSRXugUgGY8r9ZgqpY",
+  authDomain: "fmc-test-14150.firebaseapp.com",
+  projectId: "fmc-test-14150",
+  storageBucket: "fmc-test-14150.appspot.com",
+  messagingSenderId: "636475170145",
+  appId: "1:636475170145:web:389d4fb749693008937920"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
