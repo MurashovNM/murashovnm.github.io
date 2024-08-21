e4bf1ce6fc97acc1fb3e56c9a8147091fe0ddbc9
Nikita Murashov <nmurashov@sdvor.com>
2024-08-21 15:13:19 +0500

    Test 1

--- app.js
+++ app.js
@@ -11,80 +11,290 @@ var firebaseConfig = {
 // Initializing Firebase App
 firebase.initializeApp(firebaseConfig);
 
-// Initializing Cloud Messanging
-const messaging = firebase.messaging();
+var bt_register = $('#register');
+var bt_delete = $('#delete');
+var token = $('#token');
+var form = $('#notification');
+var massage_id = $('#massage_id');
+var massage_row = $('#massage_row');
 
+var info = $('#info');
+var info_message = $('#info-message');
 
-// Registering Service worker
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js').then(res => {
-        console.log("Register Success");
-        messaging.useServiceWorker(res);
-    }).catch(e => {
-        console.log(e);
+var alert = $('#alert');
+var alert_message = $('#alert-message');
+
+var input_body = $('#body');
+var timerId = setInterval(setNotificationDemoBody, 10000);
+
+function setNotificationDemoBody() {
+    if (input_body.val().search(/^It's sent today at \d\d:\d\d$/i) !== -1) {
+        var now = new Date();
+        input_body.val('It\'s sent today at ' + now.getHours() + ':' + addZero(now.getMinutes()));
+    } else {
+        clearInterval(timerId);
+    }
+}
+
+function addZero(i) {
+    return i > 9 ? i : '0' + i;
+}
+
+setNotificationDemoBody();
+resetUI();
+
+if (
+    'Notification' in window &&
+    'serviceWorker' in navigator &&
+    'localStorage' in window &&
+    'fetch' in window &&
+    'postMessage' in window
+) {
+    var messaging = firebase.messaging();
+
+    // already granted
+    if (Notification.permission === 'granted') {
+        getToken();
+    }
+
+    // get permission on subscribe only once
+    bt_register.on('click', function() {
+        getToken();
+    });
+
+    bt_delete.on('click', function() {
+        // Delete Instance ID token.
+        messaging.getToken()
+            .then(function(currentToken) {
+                messaging.deleteToken(currentToken)
+                    .then(function() {
+                        console.log('Token deleted');
+                        setTokenSentToServer(false);
+                        // Once token is deleted update UI.
+                        resetUI();
+                    })
+                    .catch(function(error) {
+                        showError('Unable to delete token', error);
+                    });
+            })
+            .catch(function(error) {
+                showError('Error retrieving Instance ID token', error);
+            });
+    });
+
+    // handle catch the notification on current page
+    messaging.onMessage(function(payload) {
+
+        console.log('Message received', payload);
+
+        if (payload.data.action && payload.data.action === 'close') {
+            console.log('Message received', 'CLOSE');
+        } else {
+            info.show();
+            info_message
+                .text('')
+                .append('<strong>'+payload.data.title+'</strong>')
+                .append('<em>'+payload.data.body+'</em>');
+
+            // register fake ServiceWorker for show notification on mobile devices
+            navigator.serviceWorker.register('/sw.js');
+            Notification.requestPermission(function(permission) {
+                if (permission === 'granted') {
+                    navigator.serviceWorker.ready.then(function(registration) {
+                      // Copy data object to get parameters in the click handler
+                      payload.data.data = JSON.parse(JSON.stringify(payload.data));
+
+                      registration.showNotification(payload.data.title, payload.data);
+                    }).catch(function(error) {
+                        // registration failed :(
+                        showError('ServiceWorker registration failed', error);
+                    });
+                }
+            });
+        }
+
+    });
+
+    // Callback fired if Instance ID token is updated.
+    messaging.onTokenRefresh(function() {
+        messaging.getToken()
+            .then(function(refreshedToken) {
+                console.log('Token refreshed');
+                // Send Instance ID token to app server.
+                sendTokenToServer(refreshedToken);
+                updateUIForPushEnabled(refreshedToken);
+            })
+            .catch(function(error) {
+                showError('Unable to retrieve refreshed token', error);
+            });
+    });
+
+} else {
+    if (!('Notification' in window)) {
+        showError('Notification not supported');
+    } else if (!('serviceWorker' in navigator)) {
+        showError('ServiceWorker not supported');
+    } else if (!('localStorage' in window)) {
+        showError('LocalStorage not supported');
+    } else if (!('fetch' in window)) {
+        showError('fetch not supported');
+    } else if (!('postMessage' in window)) {
+        showError('postMessage not supported');
+    }
+
+    console.warn('This browser does not support desktop notification.');
+    console.log('Is HTTPS', window.location.protocol === 'https:');
+    console.log('Support Notification', 'Notification' in window);
+    console.log('Support ServiceWorker', 'serviceWorker' in navigator);
+    console.log('Support LocalStorage', 'localStorage' in window);
+    console.log('Support fetch', 'fetch' in window);
+    console.log('Support postMessage', 'postMessage' in window);
+
+    updateUIForPushPermissionRequired();
+}
+
+function getToken() {
+    messaging.requestPermission()
+        .then(function() {
+            // Get Instance ID token. Initially this makes a network call, once retrieved
+            // subsequent calls to getToken will return from cache.
+            messaging.getToken()
+                .then(function(currentToken) {
+
+                    if (currentToken) {
+                        sendTokenToServer(currentToken);
+                        updateUIForPushEnabled(currentToken);
+                    } else {
+                        showError('No Instance ID token available. Request permission to generate one');
+                        updateUIForPushPermissionRequired();
+                        setTokenSentToServer(false);
+                    }
+                })
+                .catch(function(error) {
+                    showError('An error occurred while retrieving token', error);
+                    updateUIForPushPermissionRequired();
+                    setTokenSentToServer(false);
+                });
+        })
+        .catch(function(error) {
+            showError('Unable to get permission to notify', error);
+        });
+}
+
+// Send the Instance ID token your application server, so that it can:
+// - send messages back to this app
+// - subscribe/unsubscribe the token from topics
+function sendTokenToServer(currentToken) {
+    if (!isTokenSentToServer(currentToken)) {
+        console.log('Sending token to server...');
+        // send current token to server
+        //$.post(url, {token: currentToken});
+        setTokenSentToServer(currentToken);
+    } else {
+        console.log('Token already sent to server so won\'t send it again unless it changes');
+    }
+}
+
+function isTokenSentToServer(currentToken) {
+    return window.localStorage.getItem('sentFirebaseMessagingToken') === currentToken;
+}
+
+function setTokenSentToServer(currentToken) {
+    if (currentToken) {
+        window.localStorage.setItem('sentFirebaseMessagingToken', currentToken);
+    } else {
+        window.localStorage.removeItem('sentFirebaseMessagingToken');
+    }
+}
+
+function updateUIForPushEnabled(currentToken) {
+    console.log(currentToken);
+    token.text(currentToken);
+    bt_register.hide();
+    bt_delete.show();
+    form.show();
+}
+
+function resetUI() {
+    token.text('');
+    bt_register.show();
+    bt_delete.hide();
+    form.hide();
+    massage_row.hide();
+    info.hide();
+}
+
+function updateUIForPushPermissionRequired() {
+    bt_register.attr('disabled', 'disabled');
+    resetUI();
+}
+
+function showError(error, error_data) {
+    alert.show();
+
+    if (typeof error_data !== "undefined") {
+        alert_message.html(error + '<br><pre>' + JSON.stringify(error_data) + '</pre>');
+        console.error(error, error_data);
+    } else {
+        alert_message.html(error);
+        console.error(error);
+    }
+}
+
+navigator.serviceWorker.addEventListener('message', function(event) {
+    var data = event.data["firebase-messaging-msg-data"].data
+
+    if (data && data.action && data.action === 'close') {
+        closeNotification(event.data["firebase-messaging-msg-data"].data.push_id);
+    }
+});
+
+function closeNotification(id) {
+    navigator.serviceWorker.ready.then(function(reg) {
+        reg.getNotifications().then(function(notifications) {
+            for (let i = 0; i < notifications.length; i += 1) {
+
+                console.log('ntf', notifications[i])
+
+                if (
+                    notifications[i].data &&
+                    notifications[i].data.push_id &&
+                    notifications[i].data.push_id === id
+                ) {
+                    notifications[i].close();
+                }
+            }
+        });
     });
 }
 
+
+// Registering Service worker
+//if ('serviceWorker' in navigator) {
+//    navigator.serviceWorker.register('./sw.js').then(res => {
+//        console.log("Register Success");
+//        messaging.useServiceWorker(res);
+//    }).catch(e => {
+//        console.log(e);
+//    });
+//}
+
 // Subscribing to push Notifications
-const subscribe = () => {
-    const token = document.getElementById('token');
-    // getting PushNotification permission from browser
-    Notification.requestPermission().then(permission => {
-        if (permission == "granted") {
-            // if Permission is allowed then getting firebase messanging token from firebase
-            messaging.getToken().then(currentToken => {
-                console.log(currentToken);
-                // displaying token in index file
-                token.textContent = currentToken;
-            });
-        } else {
-            token.textContent = "Permission not granted";
-        }
-    }).catch(e=>{
-        token.textContent=e;
-    });
-};
-
-
-// Sending Push Notifications
-const sendPush = () => {
-    // Getting From Data when button clicked
-    const token = document.getElementById('usertoken').value;
-    const notificationTitle = document.getElementById('title').value;
-    const notificationBody = document.getElementById('body').value;
-
-    // Adding data to payload for sending push notifications
-    let body = {
-        to: token,
-        notification: {
-            title: notificationTitle,
-            body: notificationBody,
-            click_action: "/",
-        }
-    };
-
-    // Setting options for push notification
-    const options = {
-        method: "POST",
-        headers: new Headers({
-            // Add your server key after key=
-            Authorization: "key=AAAAuIQZHXs:APA91bF6OzMEeWl7aP0DSgGdZDIvR6JSjhvigHSF9Tb5dxmXP_-5TwhlhHqgcol0H8r6EbZRL0Mfp8eDT65S6YYH1shZiviwzFhZnJGlaDYL42rjqeBnWBEdtT9tJzl_CZ4U1HLevIsT",
-            "Content-Type": "application/json"
-        }),
-        body: JSON.stringify(body)
-    };
-
-    // Sending Push notifications to user using fetch api
-    fetch("https://fcm.googleapis.com/fcm/send", options)
-        .then(res => res.json())
-        .then(data => {
-            if (data.failure == 1) {
-                alert("Token Expire");
-            } else {
-                alert("Send Success");
-            }
-        })
-        .catch(err => {
-            alert(err);
-        });
-};
+//const subscribe = () => {
+//    const token = document.getElementById('token');
+//    // getting PushNotification permission from browser
+//    Notification.requestPermission().then(permission => {
+//        if (permission == "granted") {
+//            // if Permission is allowed then getting firebase messanging token from firebase
+//            messaging.getToken().then(currentToken => {
+//                console.log(currentToken);
+//                // displaying token in index file
+//                token.textContent = currentToken;
+//            });
+//        } else {
+//            token.textContent = "Permission not granted";
+//        }
+//    }).catch(e=>{
+//        token.textContent=e;
+//    });
+//};
--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/7.14.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.14.0/firebase-messaging.js');
+
 var firebaseConfig = {
   apiKey: "AIzaSyCRXnah5DNJzyr6DhSRXugUgGY8r9ZgqpY",
   authDomain: "fmc-test-14150.firebaseapp.com",
@@ -11,3 +12,76 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
+
+// Customize notification handler
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('Handling background message', payload);
+
+  // Copy data object to get parameters in the click handler
+  payload.data.data = JSON.parse(JSON.stringify(payload.data));
+
+  return self.registration.showNotification(payload.data.title, payload.data);
+});
+
+self.addEventListener('notificationclick', function(event) {
+  const target = event.notification.data.click_action || '/';
+  event.notification.close();
+
+  // This looks to see if the current is already open and focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: 'window',
+    includeUncontrolled: true
+  }).then(function(clientList) {
+    // clientList always is empty?!
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if (client.url === target && 'focus' in client) {
+        return client.focus();
+      }
+    }
+
+    return clients.openWindow(target);
+  }));
+});
+
+//self.addEventListener('message', function(event) {
+//  let opts = event.data.json();
+//
+//  if (opts.data && opts.data.action) {
+//    event.waitUntil(
+//      clients.matchAll().then(
+//        function(windowClients) {
+//          for (let i = 0; i < windowClients.length; i += 1) {
+//            windowsClients[i].postMessage(opts.data);
+//          }
+//        }
+//      )
+//    );
+//  }
+//});
+
+
+self.addEventListener('push', function(event) {
+    console.log('GET', 'push')
+    console.log('EVENT', event)
+//    const analyticsPromise = pushReceivedTracking();
+//    const pushInfoPromise = fetch('/api/get-more-data')
+//    .then(function(response) {
+//        return response.json();
+//    })
+//    .then(function(response) {
+//        const title = response.data.userName + ' says...';
+//        const message = response.data.message;
+//
+//        self.registration.showNotification(title, {
+//        body: message
+//        });
+//    });
+//
+//    const promiseChain = Promise.all([
+//    analyticsPromise,
+//    pushInfoPromise
+//    ]);
+//
+//    event.waitUntil(promiseChain);
+});
